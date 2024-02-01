7882487552efe22d6a47c5815e2c989db0d93a7e
Ednei <edneitrabach@hotmail.com>
2024-02-01 17:47:59 -0300

    Finalização tópico

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,65 @@
+import java.sql.SQLOutput;
 import java.text.NumberFormat;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        int valorCarro = 100_000;
-        if (valorCarro > 100_00)
-            System.out.println("Não comprar! Acima da tabela.");
+        // PRÁTICA
+
+        // O INPUT
+        Scanner lerNumeros = new Scanner(System.in);
+        System.out.print("Digite um numero: ");
+        int numero = lerNumeros.nextInt();
+
+        // O CODIGO
+        if (numero <9 && numero >= 1){
+            System.out.println("Este número tem 1 digito");
+        } else if (numero <100 && numero >= 10) {
+            System.out.println("Este número tem 2 digitos");
+        } else if (numero <1000 && numero >= 100) {
+            System.out.println("Este número tem 3 digitos");
+        } else if (numero <10000 && numero >= 1000) {
+            System.out.println("Este número tem 4 digitos");
+        } else if (numero <100000 && numero >= 10000) {
+            System.out.println("Este número tem 5 digitos");
+        } else {
+            System.out.println("Este número tem 5 digitos ou mais");
+        }
 
 
         /***
+
+
+         // OPERADOR TERNÁRIO
+         int ingresso = 499;
+         String terAcesso = ingresso >= 500 ? "VIP" : "Pista"; // OPERADOR TERNÁRIO
+         System.out.println(terAcesso);
+
+
+
+         int ingresso = 800;
+         String terAcesso = "Pista"; // COLOCANDO O VALOR DEFAULT
+         if (ingresso >= 500 )
+         terAcesso = "VIP";
+         System.out.println(terAcesso);
+
+
+         int ingresso = 800;
+         String terAcesso;
+         if (ingresso >= 500)
+         terAcesso = "VIP";
+         else
+         terAcesso = "Pista";
+         System.out.println(terAcesso);
+
+
+         int valorCarro = 100_000;
+         if (valorCarro > 100_000)
+         System.out.println("Não comprar! Acima da tabela.");
+         else if (valorCarro >= 90_000 && valorCarro <= 100_000)
+         System.out.println("Pode comprar!!");
+         else
+         System.out.println("Não comprar! Valor  abaixo da tabela.");
 
          boolean temEmprego = true;
          boolean temEmpresa = false;
